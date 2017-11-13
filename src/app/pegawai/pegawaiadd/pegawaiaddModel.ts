aa0d68a146ec2894802bb4b6b3cc6b06ae276704
TOSHIBA-PC\TOSHIBA <egiwibowo13@gmaill.com>
2017-11-14 06:32:29 +0700

    no message

--- src/app/pegawai/pegawaiadd/pegawaiaddModel.ts
+++
@@ -1,8 +0,0 @@
-export class Data {
-    public _id : string;
-    public UserNamePegawai : string;
-    public PasswordPegawai : string;
-    public NamaPegawai : string;
-    public AlamatPegawai : string;
-    public NoTelp : string;
-}
