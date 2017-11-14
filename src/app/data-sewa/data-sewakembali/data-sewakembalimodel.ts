2016ce400e9f2c043502bb47feef3af4d9c462a3
TOSHIBA-PC\TOSHIBA <egiwibowo13@gmaill.com>
2017-11-14 07:24:18 +0700

    no message

--- src/app/data-sewa/data-sewakembali/data-sewakembalimodel.ts
+++
@@ -1,7 +0,0 @@
-export class Data {
-   public KdDenda : String;
-   public KdDataSewa : String;
-   public KdBarang : String;
-   public JumlahDenda : Number;
-   public StatusDenda : String;
-}
--- src/app/penyewa/penyewaadd/penyewaaddModel.ts
+++
@@ -1,9 +0,0 @@
-export class Data {
-    public _id : string;
-    public UserNamePenyewa : string;
-    public PasswordPenyewa : string;
-    public NamaPenyewa : string;
-    public EmailPenyewa : string;
-    public AlamatPenyewa : string;
-    public NoTelp : string;
-}
