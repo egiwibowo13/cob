0ebb9a68e99bacb6157bfa75fabe90e7758bf320
TOSHIBA-PC\TOSHIBA <egiwibowo13@gmaill.com>
2017-11-14 07:16:52 +0700

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,69 @@ import { IndexfooterComponent } from './indexfooter/indexfooter.component';
 import { IndexsidebarComponent } from './indexsidebar/indexsidebar.component';
 import { SearchComponent } from './search/search.component';
 import { LoginComponent } from './login/login.component';
+import { BarangComponent } from './barang/barang.component';
+import { BarangaddComponent } from './barang/barangadd/barangadd.component';
+import { BarangdetailComponent } from './barang/barangdetail/barangdetail.component';
+import { BarangeditComponent } from './barang/barangedit/barangedit.component';
+import { PenyewaComponent } from './penyewa/penyewa.component';
+import { PenyewaaddComponent } from './penyewa/penyewaadd/penyewaadd.component';
+import { PenyewadetailComponent } from './penyewa/penyewadetail/penyewadetail.component';
+import { PenyewaeditComponent } from './penyewa/penyewaedit/penyewaedit.component';
+import { DataSewaComponent } from './data-sewa/data-sewa.component';
+import { DataSewaaddComponent } from './data-sewa/data-sewaadd/data-sewaadd.component';
+import { DataSewadetailComponent } from './data-sewa/data-sewadetail/data-sewadetail.component';
+import { DataSewaeditComponent } from './data-sewa/data-sewaedit/data-sewaedit.component';
+import { DendaComponent } from './denda/denda.component';
+import { DendaaddComponent } from './denda/dendaadd/dendaadd.component';
+import { DendadetailComponent } from './denda/dendadetail/dendadetail.component';
+import { DendaeditComponent } from './denda/dendaedit/dendaedit.component';
+import { KategoriComponent } from './kategori/kategori.component';
+import { KategoriaddComponent } from './kategori/kategoriadd/kategoriadd.component';
+import { KategoridetailComponent } from './kategori/kategoridetail/kategoridetail.component';
+import { KategorieditComponent } from './kategori/kategoriedit/kategoriedit.component';
+import { PegawaiComponent } from './pegawai/pegawai.component';
+import { PegawaiaddComponent } from './pegawai/pegawaiadd/pegawaiadd.component';
+import { PegawaidetailComponent } from './pegawai/pegawaidetail/pegawaidetail.component';
+import { PegawaieditComponent } from './pegawai/pegawaiedit/pegawaiedit.component';
+import { PembayaranComponent } from './pembayaran/pembayaran.component';
+import { PembayaranaddComponent } from './pembayaran/pembayaranadd/pembayaranadd.component';
+import { PembayarandetailComponent } from './pembayaran/pembayarandetail/pembayarandetail.component';
+import { PembayaraneditComponent } from './pembayaran/pembayaranedit/pembayaranedit.component';
+import { DataSewakembaliComponent} from './data-sewa/data-sewakembali/data-sewakembali.component';
 
 
 let routes : Routes=[
   {path : '',component:SearchComponent},
   {path:'login',component:LoginComponent},
+  {path:'barang',component:BarangComponent},
+  {path:'barang/add',component:BarangaddComponent},
+  {path:'barang/detail/:id',component:BarangdetailComponent},
+  {path:'barang/edit/:id',component:BarangeditComponent},
+  {path:'penyewa',component:PenyewaComponent},
+  {path:'penyewa/add',component:PenyewaaddComponent},
+  {path:'penyewa/detail/:id',component:PenyewadetailComponent},
+  {path:'penyewa/edit/:id',component:PenyewaeditComponent},
+  {path:'datasewa',component:DataSewaComponent},
+  {path:'datasewa/add',component:DataSewaaddComponent},
+  {path:'datasewa/detail/:id',component:DataSewadetailComponent},
+  {path:'datasewa/edit/:id',component:DataSewaeditComponent},
+  {path:'denda',component:DendaComponent},
+  {path:'denda/add',component:DendaaddComponent},
+  {path:'denda/detail/:id',component:DendadetailComponent},
+  {path:'denda/edit/:id',component:DendaeditComponent},
+  {path:'kategori',component:KategoriComponent},
+  {path:'kategori/add',component:KategoriaddComponent},
+  {path:'kategori/detail/:id',component:KategoridetailComponent},
+  {path:'kategori/edit/:id',component:KategorieditComponent},
+  {path:'pegawai',component:PegawaiComponent},
+  {path:'pegawai/add',component:PegawaiaddComponent},
+  {path:'pegawai/detail/:id',component:PegawaidetailComponent},
+  {path:'pegawai/edit/:id',component:PegawaieditComponent},
+  {path:'pembayaran',component:PembayaranComponent},
+  {path:'pembayaran/add',component:PembayaranaddComponent},
+  {path:'pembayaran/detail/:id',component:PembayarandetailComponent},
+  {path:'pembayaran/edit/:id',component:PembayaraneditComponent},
+  {path:'datasewa/kembali/:id',component:DataSewakembaliComponent}
 ]
 
 
@@ -28,7 +86,36 @@ let routes : Routes=[
     IndexfooterComponent,
     IndexsidebarComponent,  
     SearchComponent,
-    LoginComponent
+    LoginComponent,
+    BarangComponent,
+    BarangaddComponent,
+    BarangdetailComponent,
+    BarangeditComponent,
+    PenyewaComponent,
+    PenyewaaddComponent,
+    PenyewadetailComponent,
+    PenyewaeditComponent,
+    DataSewaComponent,
+    DataSewaaddComponent,
+    DataSewadetailComponent,
+    DataSewaeditComponent,
+    DataSewakembaliComponent,
+    DendaComponent,
+    DendaaddComponent,
+    DendadetailComponent,
+    DendaeditComponent,
+    KategoriComponent,
+    KategoriaddComponent,
+    KategoridetailComponent,
+    KategorieditComponent,
+    PegawaiComponent,
+    PegawaiaddComponent,
+    PegawaidetailComponent,
+    PegawaieditComponent,
+    PembayaranComponent,
+    PembayaranaddComponent,
+    PembayarandetailComponent,
+    PembayaraneditComponent
    ],
   imports: [
     BrowserModule,
