2d364fd77df1124caac110c5ced5bcdd505abdc9
TOSHIBA-PC\TOSHIBA <egiwibowo13@gmaill.com>
2017-11-14 14:03:40 +0700

    no message

--- src/app/data-sewa/data-sewakembali/data-sewakembali.component.ts
+++ src/app/data-sewa/data-sewakembali/data-sewakembali.component.ts
@@ -52,41 +52,41 @@ export class DataSewakembaliComponent implements OnInit {
       })
   }
 
-  datasewaKembali(id){
-    this.dataDenda.KdDenda="";
-    this.dataDenda.KdDataSewa=this.dataDetail.KdDataSewa;
-    this.dataDenda.KdBarang=this.dataBarang[0].KdBarang;
-    this.dataDenda.JumlahDenda= this.denda;
+  // datasewaKembali(id){
+  //   this.dataDenda.KdDenda="";
+  //   this.dataDenda.KdDataSewa=this.dataDetail.KdDataSewa;
+  //   this.dataDenda.KdBarang=this.dataBarang[0].KdBarang;
+  //   this.dataDenda.JumlahDenda= this.denda;
     
-    this.dataDenda.StatusDenda="Lunas";
-    let header = new Headers({'Content-Type':'application/json'});
-    let opsi = new RequestOptions({headers:header});
-    let data = JSON.stringify(this.dataDenda);
+  //   this.dataDenda.StatusDenda="Lunas";
+  //   let header = new Headers({'Content-Type':'application/json'});
+  //   let opsi = new RequestOptions({headers:header});
+  //   let data = JSON.stringify(this.dataDenda);
     
-    if(this.dataDenda.JumlahDenda>0){
-    this.http.post('https://penyewaanbatch124.herokuapp.com/api/denda?token='+localStorage.getItem("token"),data,opsi)
-    .subscribe((res:Response)=>{
-      window.location.href='./datasewa';
-    })}
+  //   if(this.dataDenda.JumlahDenda>0){
+  //   this.http.post('https://penyewaanbatch124.herokuapp.com/api/denda?token='+localStorage.getItem("token"),data,opsi)
+  //   .subscribe((res:Response)=>{
+  //     window.location.href='./datasewa';
+  //   })}
 
-    this.dataDetail.StatusDataSewa = "selesai";
-    this.http.put('https://penyewaanbatch124.herokuapp.com/api/datasewa/'+this.id+'?token='+localStorage.getItem("token"),this.dataDetail)
-    .subscribe((res:Response)=>{
-     window.location.href='./datasewa';
+  //   this.dataDetail.StatusDataSewa = "selesai";
+  //   this.http.put('https://penyewaanbatch124.herokuapp.com/api/datasewa/'+this.id+'?token='+localStorage.getItem("token"),this.dataDetail)
+  //   .subscribe((res:Response)=>{
+  //    window.location.href='./datasewa';
      
-    })
+  //   })
 
-    this.dataBarang[0].JumlahBarang = this.dataBarang[0].JumlahBarang+this.dataDetail.JumlahBarang;
-    this.dataBarang[0].StatusBarang = "Available"
-    console.log(this.idBarang);
-    debugger;
-    this.http.put('https://penyewaanbatch124.herokuapp.com/api/barang/'+this.idBarang,this.dataBarang[0])
-    .subscribe((res:Response)=>{
-     window.location.href='./datasewa';
+  //   this.dataBarang[0].JumlahBarang = this.dataBarang[0].JumlahBarang+this.dataDetail.JumlahBarang;
+  //   this.dataBarang[0].StatusBarang = "Available"
+  //   console.log(this.idBarang);
+  //   debugger;
+  //   this.http.put('https://penyewaanbatch124.herokuapp.com/api/barang/'+this.idBarang,this.dataBarang[0])
+  //   .subscribe((res:Response)=>{
+  //    window.location.href='./datasewa';
      
-    })
+  //   })
 
-  }
+  // }
 }
 
 
